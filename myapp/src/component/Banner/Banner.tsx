5fe5f69512cfbd7727844a49c269547812c77617
vu191204 <vule90008@gmail.com>
2024-11-01 11:53:20 +0700

    updatene

---
+++ myapp/src/component/Banner/Banner.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Banner.scss";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div>
+        <img
+          src="./image/banner3.png"
+          alt="Miss Dior Banner 1"
+          className="banner-image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
