4a2d33a91a5dab705d585e276e78238558f77aab
vulv1204 <vulv04.dev@gmail.com>
2024-11-11 21:07:03 +0700

    update_client_new

--- resources/views/client/pages/About/about.blade.php
+++ resources/views/client/pages/About/about.blade.php
@@ -11,28 +11,28 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}">
 
     <!-- CSS
 	============================================ -->
     <!-- google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900" rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/bootstrap.min.css')}}">
     <!-- Pe-icon-7-stroke CSS -->
-    <link rel="stylesheet" href="assets/css/vendor/pe-icon-7-stroke.css">
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/pe-icon-7-stroke.css')}}">
     <!-- Font-awesome CSS -->
-    <link rel="stylesheet" href="assets/css/vendor/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/font-awesome.min.css')}}">
     <!-- Slick slider css -->
-    <link rel="stylesheet" href="assets/css/plugins/slick.min.css">
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/slick.min.css')}}">
     <!-- animate css -->
-    <link rel="stylesheet" href="assets/css/plugins/animate.css">
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/animate.css')}}">
     <!-- Nice Select css -->
-    <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/nice-select.css')}}">
     <!-- jquery UI css -->
-    <link rel="stylesheet" href="assets/css/plugins/jqueryui.min.css">
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/jqueryui.min.css')}}">
     <!-- main style css -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 
 </head>
 
@@ -1012,33 +1012,33 @@
 ============================================ -->
 
     <!-- Modernizer JS -->
-    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="{{asset('assets/js/vendor/modernizr-3.6.0.min.js')}}"></script>
     <!-- jQuery JS -->
-    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="{{asset('assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
     <!-- Bootstrap JS -->
-    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
     <!-- slick Slider JS -->
-    <script src="assets/js/plugins/slick.min.js"></script>
+    <script src="{{asset('assets/js/plugins/slick.min.js')}}"></script>
     <!-- Countdown JS -->
-    <script src="assets/js/plugins/countdown.min.js"></script>
+    <script src="{{asset('assets/js/plugins/countdown.min.js')}}"></script>
     <!-- Nice Select JS -->
-    <script src="assets/js/plugins/nice-select.min.js"></script>
+    <script src="{{asset('assets/js/plugins/nice-select.min.js')}}"></script>
     <!-- jquery UI JS -->
-    <script src="assets/js/plugins/jqueryui.min.js"></script>
+    <script src="{{asset('assets/js/plugins/jqueryui.min.js')}}"></script>
     <!-- Image zoom JS -->
-    <script src="assets/js/plugins/image-zoom.min.js"></script>
+    <script src="{{asset('assets/js/plugins/image-zoom.min.js')}}"></script>
     <!-- Images loaded JS -->
-    <script src="assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+    <script src="{{asset('assets/js/plugins/imagesloaded.pkgd.min.js')}}"></script>
     <!-- mail-chimp active js -->
-    <script src="assets/js/plugins/ajaxchimp.js"></script>
+    <script src="{{asset('assets/js/plugins/ajaxchimp.js')}}"></script>
     <!-- contact form dynamic js -->
-    <script src="assets/js/plugins/ajax-mail.js"></script>
+    <script src="{{asset('assets/js/plugins/ajax-mail.js')}}"></script>
     <!-- google map api -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfmCVTjRI007pC1Yk2o2d_EhgkjTsFVN8"></script>
     <!-- google map active js -->
-    <script src="assets/js/plugins/google-map.js"></script>
+    <script src="{{asset('assets/js/plugins/google-map.js')}}"></script>
     <!-- Main JS -->
-    <script src="assets/js/main.js"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
 </body>
 
 
