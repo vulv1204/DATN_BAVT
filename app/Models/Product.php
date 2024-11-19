d125ee9cfc18e16ea6fdd7bd17c54e40e55b5304
Hoàng Anh <dev.hoanganh333@gmail.com>
2024-11-19 07:56:43 +0700

    Product/fix model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -41,6 +41,6 @@ class Product extends Model
     // Thiết lập quan hệ với model `Category`
     public function categories()
     {
-        return $this->belongsToMany(Category::class, 'category_products', 'product_id', 'category_id');
+        return $this->belongsToMany(Category::class, 'category_product', 'product_id', 'category_id');
     }
 }
