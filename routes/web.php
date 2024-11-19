2382d129e5b7bc0dc2ab5dda5735d9dc23733d2f
vulv1204 <vulv04.dev@gmail.com>
2024-11-19 12:10:49 +0700

    update_client/19/11

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('home');
 });
-
 
 
 Route::resource('categories', CategoryController::class);
